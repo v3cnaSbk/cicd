dfb36f0de7e38c0282e2b792392bf3490896bfbb
Dhiraj <elixir@DHIRAJ-KUMAR.local>
2025-04-22 21:57:52 +0530

    First push

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def hello_user(username):
     return render_template('index.html',message=username+": "+result,imgname="jenkins")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=8080)
